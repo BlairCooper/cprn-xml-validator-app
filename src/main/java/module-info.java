f8ce5ecd08a3b7782569717b04e7d30b3fa95d25
Blair Cooper <dev@raincitysolutions.com>
2021-05-04 20:09:22 -0700

    Remove javax.xml.debug

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module cprn.xml.validator.app {
     requires java.logging;
 	requires java.xml;
-	requires jdk.crypto.cryptoki;
+	requires jdk.crypto.cryptoki;	// Needed for SSL support, fetching XSDs from server
 	requires cprn.xml.validator;
 }
