894bdf8319a196756f15ab028ff607fae5e1fe1f
Blair Cooper <dev@raincitysolutions.com>
2022-01-21 12:03:48 -0800

    Rev cprn-xml-validator to .20, use Text-IO

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module cprn.xml.validator.app {
+	requires java.desktop;			// Needed for JFileChooser
     requires java.logging;
 	requires java.xml;
 	requires jdk.crypto.cryptoki;	// Needed for SSL support, fetching XSDs from server
+	requires org.beryx.textio;
 	requires cprn.xml.validator;
 }
