0d5cfc1001e64a1d3cadd641329f644cd630992c
Blair Cooper <dev@raincitysolutions.com>
2022-01-21 12:04:27 -0800

    Make use of Text-IO, and JFileChooser when using a Swing terminal

--- src/main/java/org/cprn/validator/app/Main.java
+++ src/main/java/org/cprn/validator/app/Main.java
@@ -1,31 +1,66 @@
 package org.cprn.validator.app;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-import java.io.InputStreamReader;
+import java.util.Map.Entry;
+import java.util.Properties;
 
+import javax.swing.JFileChooser;
+
+import org.beryx.textio.TerminalProperties;
+import org.beryx.textio.TextIO;
+import org.beryx.textio.TextIoFactory;
+import org.beryx.textio.TextTerminal;
+import org.beryx.textio.swing.SwingTextTerminal;
 import org.cprn.validator.CprnXmlValidator;
 
 public class Main {
+	private static final String INPUT_FILE_PROMPT_BOOKMARK = "filePrompt";
+	
 	private boolean printedUsage = false;
 
-	public static void main(String[] args) {
+	protected TextIO textIO;
+	protected TextTerminal<?> terminal;
+
+	public static void main(String[] args) throws IOException {
 		new Main().run(args);
 	}
 	
+	public Main() throws IOException {
+		textIO = TextIoFactory.getTextIO();
+		terminal = textIO.getTextTerminal();
+		
+		TerminalProperties<?> props = terminal.getProperties();
+		
+		Properties props2 = new Properties();
+		props2.load(getClass().getResourceAsStream("/textio.properties"));
+		
+		for (Entry<Object, Object> prop : props2.entrySet()) {
+			props.put((String) prop.getKey(), prop.getValue());
+		}
+	}
+
 	public void run(String[] args) {
 		boolean bErrorsFound = false;
+		boolean interactiveMode = false;
+
+		displayApplicationInformation(terminal);		
 
 		List<String> argList = Arrays.asList(args);
 
-		if (argList.size() == 0) {
-			printUsage(true);
+		if (0 == argList.size()) {
+			interactiveMode = true;
+			printUsage();
 			argList = readArgsFromConsole();
 		}
 
@@ -33,26 +68,34 @@ public class Main {
 		List<File> fileList = buildFileList(argList);
 
 		for(File file : fileList) {
+			terminal.println(String.format("Validating %s", file.toString()));
 			bErrorsFound |= !CprnXmlValidator.validateFile(file);
 			fileCnt++;
 		}
 
 		if (fileCnt > 0) {
-			System.out.println("Processed " + fileCnt + " files.");
+			terminal.println("Processed " + fileCnt + " files.");
 		} else {
 			printUsage();
 		}
 		
+		if (interactiveMode) {
+			// We are in interactive mode so pause before exiting
+			textIO
+				.newStringInputReader()
+				.withMinLength(0)
+				.withPropertiesPrefix("info")
+				.read("Press Enter to exit...");
+		}
+
+		textIO.dispose();
+
 		System.exit(bErrorsFound ? 1 : 0);
 	}
 	
 	private void printUsage() {
-		printUsage(false);
-	}
-
-	private void printUsage(boolean isInteractive) {
 		if (!printedUsage) {
-			System.out.println(
+			terminal.println(
 					new StringBuilder()
 					.append("Usage:").append(System.lineSeparator())
 					.append("\tInput files and/or folders can be provided on the command line or entered when prompted.").append(System.lineSeparator())
@@ -66,7 +109,9 @@ public class Main {
 
 	private List<String> readArgsFromConsole() {
 		List<String> argList = new ArrayList<String>();
+    	boolean showError = false;
 
+/*
 		try (BufferedReader reader = new BufferedReader(
 	            new InputStreamReader(System.in)) )
 		{
@@ -82,8 +127,53 @@ public class Main {
 			}
 		}
 		catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
+			terminal.println("Error: " + e.getMessage());
 		}
+*/
+
+    	terminal.setBookmark(INPUT_FILE_PROMPT_BOOKMARK);
+
+    	while (0 == argList.size()) {
+			String[] args = {};
+
+    		if (showError) {
+    			terminal.println("Unable to access file(s) or directory.");
+    		}
+
+    		if (isSwingTerminal()) {
+    			SwingTextTerminal swingTerminal = (SwingTextTerminal) terminal;
+    			
+    			 JFileChooser chooser = new JFileChooser();
+            	 chooser.setDialogTitle("CPRN XML Validation Tool");
+            	 if (chooser.showSaveDialog(swingTerminal.getFrame()) == JFileChooser.APPROVE_OPTION) {
+            		 final File fileLocn = chooser.getSelectedFile();
+            		 args = new String[] {fileLocn.toString()};
+            	 }
+    		}
+    		else {
+				String inputLine = textIO.newStringInputReader()
+						.withMinLength(0)	// use 0 length so we can make use of our bookmark
+				        .read("Enter files and/or folders to validate:");
+				args = inputLine.split(" ");
+    		}
+
+			for(String arg : args) {
+				
+				arg = arg.trim();
+				
+				if (arg.length() > 0) {
+					Path path = Paths.get(arg);
+					if (Files.exists(path)) {
+						if (Files.isReadable(path) ) {
+							argList.add(arg);
+						}
+					}
+				}
+			}
+
+			terminal.resetToBookmark(INPUT_FILE_PROMPT_BOOKMARK);
+	    	showError = true;
+    	}
 
 		return argList;
 	}
@@ -114,10 +204,28 @@ public class Main {
 				fileList.add(src);
 			}
 			else if (!src.exists()) {
-				System.out.println(String.format("The specified file (%s) doees not exist", arg));
+				terminal.println(String.format("The specified file (%s) doees not exist", arg));
 			}
 		}
 
 		return fileList;
 	}
+
+	/**
+	 * Display the application information.
+	 */
+    private void displayApplicationInformation(TextTerminal<?> terminal) {
+    	try (InputStream inStrm = Main.class.getResourceAsStream("/version.txt")) {
+			if (null != inStrm) {
+				String appStr = new String(inStrm.readAllBytes(), StandardCharsets.UTF_8);
+
+				terminal.println(appStr);
+			}
+		} catch (IOException e) {
+		}
+    }
+    
+    private boolean isSwingTerminal() {
+    	return (terminal instanceof SwingTextTerminal);
+    }
 }
