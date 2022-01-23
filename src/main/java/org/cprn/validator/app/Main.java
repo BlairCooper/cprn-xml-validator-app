df7672d1642a6969f2f717ac8f11721efc2fd70e
Blair Cooper <dev@raincitysolutions.com>
2022-01-22 21:48:51 -0800

    Changes to terminal usage

--- src/main/java/org/cprn/validator/app/Main.java
+++ src/main/java/org/cprn/validator/app/Main.java
@@ -1,5 +1,8 @@
 package org.cprn.validator.app;
 
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.HeadlessException;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
@@ -40,52 +43,61 @@ public class Main {
 		new Main().run(args);
 	}
 
-	public Main() throws IOException {
-		textIO = TextIoFactory.getTextIO();
-		terminal = textIO.getTextTerminal();
-
-		TerminalProperties<?> props = terminal.getProperties();
-
-		Properties props2 = new Properties();
-		props2.load(getClass().getResourceAsStream("/textio.properties"));
-
-		for (Entry<Object, Object> prop : props2.entrySet()) {
-			props.put((String) prop.getKey(), prop.getValue());
-		}
-
+	public Main() {
 		try {
 			propFile = new AppPropertiesFile();
 		}
 		catch (IOException ioe) {}
 	}
 
+	/**
+	 * Run the validation.
+	 * 
+	 * @param args An array of arguments from the command line if there were
+	 * 		any.
+	 */
 	public void run(String[] args) {
 		boolean bErrorsFound = false;
 		boolean interactiveMode = false;
 
+		List<String> fileFolderList = Arrays.asList(args);
+
+		// if there were not command line arguments assume we're running in
+		// interactive mode as we'll need to prompt for what to validate.
+		if (0 == fileFolderList.size()) {
+			interactiveMode = true;
+
+			if (!isHeadless()) {
+				// If we're not headless, use the Swing terminal
+				System.setProperty(TextIoFactory.TEXT_TERMINAL_CLASS_PROPERTY, "org.beryx.textio.swing.SwingTextTerminal");
+			}
+		}
+		else {
+			System.setProperty(TextIoFactory.TEXT_TERMINAL_CLASS_PROPERTY, "org.beryx.textio.jline.JLineTextTerminal");
+		}
+
+		// Now that we've determined what "mode" we're running in, initialize
+		// the terminal.
+		initializeTerminal();
+
 		displayApplicationInformation(terminal);
 
-		List<String> argList = Arrays.asList(args);
-
-		if (0 == argList.size()) {
-			interactiveMode = true;
+		if (interactiveMode) {
 			printUsage();
-			argList = readArgsFromConsole();
+			fileFolderList = readArgsFromConsole();
 		}
 
-		int fileCnt = 0;
-		List<File> fileList = buildFileList(argList);
+		List<File> fileList = buildFileList(fileFolderList);
 
-		for(File file : fileList) {
-			terminal.println(String.format("Validating %s", file.toString()));
-			bErrorsFound |= !CprnXmlValidator.validateFile(file);
-			fileCnt++;
-		}
-
-		if (fileCnt > 0) {
-			terminal.println("Processed " + fileCnt + " files.");
-		} else {
+		if (0 == fileList.size()) {
 			printUsage();
+		}
+		else {
+			for(File file : fileList) {
+				terminal.println(String.format("Validating %s", file.toString()));
+				bErrorsFound |= !CprnXmlValidator.validateFile(file);
+			}
+			terminal.println("Processed " + fileList.size() + " files.");
 		}
 
 		if (interactiveMode) {
@@ -102,13 +114,44 @@ public class Main {
 		System.exit(bErrorsFound ? 1 : 0);
 	}
 
+	/**
+	 * Initialize the terminal we'll be using.
+	 * <p>
+	 * To work around the issue that Text-IO does not support reading the
+	 * property file within a module we manually load our properties into the
+	 * terminal.
+	 * <p>
+	 * Similarly,Text-IO cannot load the icon from within our module.
+	 */
+	private void initializeTerminal() {
+		textIO = TextIoFactory.getTextIO();
+		terminal = textIO.getTextTerminal();
+
+		try {
+			TerminalProperties<?> props = terminal.getProperties();
+	
+			Properties props2 = new Properties();
+			props2.load(getClass().getResourceAsStream("/textio.properties"));
+	
+			for (Entry<Object, Object> prop : props2.entrySet()) {
+				props.put((String) prop.getKey(), prop.getValue());
+			}
+		}
+		catch (IOException ioe) {
+			
+		}
+	}
+
+	/**
+	 * Print the application usage.
+	 */
 	private void printUsage() {
 		if (!printedUsage) {
 			terminal.println(
 					new StringBuilder()
 					.append("Usage:").append(System.lineSeparator())
 					.append("\tInput files and/or folders can be provided on the command line or entered when prompted.").append(System.lineSeparator())
-					.append("\tProviding multiple files and or folders is allowed.").append(System.lineSeparator())
+					.append("\tProviding multiple files and/or folders is allowed.").append(System.lineSeparator())
 					.append(System.lineSeparator())
 					.append("\tE.g. CprnXmlValidator <input.xml> OR <folder containing XML files>").append(System.lineSeparator())
 					.toString());
@@ -116,6 +159,18 @@ public class Main {
 		}
 	}
 
+	/**
+	 * Read the file and/or folders from the terminal. The assumption is that
+	 * this method is called because no files or folder were provided on the
+	 * command line.
+	 * <p>
+	 * When the Swing terminal is being used we use the JFileChooser to select
+	 * the file or folder to validate. In the case, there will only be one.
+	 * If a Console or JLine terminal is used it is possible for the user to
+	 * provide multiple files and/or folders.
+	 * 
+	 * @return A list of files and/or folders to be validate.
+	 */
 	private List<String> readArgsFromConsole() {
 		List<String> argList = new ArrayList<String>();
 		boolean showError = false;
@@ -208,6 +263,14 @@ public class Main {
 		return argList;
 	}
 
+	/**
+	 * Build the list of files to be validated.
+	 * 
+	 * @param argList The potential files/folder provided via the command
+	 * 			line, from the terminal prompt or the JFileChoooser.
+	 *		
+	 * @return A list of files to validated.
+	 */
 	private List<File> buildFileList(List<String> argList) {
 		List<File> fileList = new LinkedList<File>();
 
@@ -255,7 +318,27 @@ public class Main {
 		}
 	}
 
+	/**
+	 * Check if the terminal being used is a Swing terminal.
+	 * 
+	 * @return Returns true if it is a Swing terminal, otherwise returns false.
+	 */
 	private boolean isSwingTerminal() {
 		return (terminal instanceof SwingTextTerminal);
 	}
+
+	/**
+	 * Check if we're running in a headless environment. 
+	 * 
+	 * @return Returns true if the environment is headless, otherwise returns false.
+	 */
+    private static boolean isHeadless() {
+        if (GraphicsEnvironment.isHeadless()) return true;
+        try {
+            GraphicsDevice[] screenDevices = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
+            return screenDevices == null || screenDevices.length == 0;
+        } catch (HeadlessException e) {
+            return true;
+        }
+    }
 }
